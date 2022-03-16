86f899a36490e73a0033d817202c02f4c1fb2555
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-03-16 15:32:14 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+window.function = function (embed) {
+
+  // data
+  embed = embed.value ?? "";
+
+  let ht = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Glide Yes-Code</title>
+  </head>
+  <body>
+${embed}
+  </body>
+</html>`
+
+  let enc = encodeURIComponent(ht);
+  let uri = `data:text/html;charset=utf-8,${enc}`
+  return uri; 
+}
